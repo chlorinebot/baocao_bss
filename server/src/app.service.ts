b588677842d994d7349b155cf8c96ef574412ca2
chatswt <kimdominic714@gmail.com>
2025-08-08 11:58:46 +0700

    g3

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! Đây là server của BSS Team từ NestJS!';
   }
 }
